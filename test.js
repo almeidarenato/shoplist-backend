8880a353d35e1674aea77cdec81dff482f774e89
almeidarenato <renatoam@ymail.com>
2020-02-21 20:55:13 -0300

    adicionado testes com mocha

---
+++ test.js
@@ -0,0 +1,12 @@
+const { deepEqual } = require("assert");
+//const ProdutoController = require("./src/controllers/ProdutoController");
+const axios = require("axios");
+
+describe("Api de Manipulação de Lista de Compras", async () => {
+  it("deve trazer a lista de itens cadastrados", async () => {
+    const expected = true;
+    const result = await axios.get("http://localhost:8082/produto");
+    if (result.data.length > 0) deepEqual(true, expected);
+    else deepEqual(false, expected, `A lista não retornou resultado`);
+  });
+});
